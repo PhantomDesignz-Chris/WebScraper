fe73cefac69bce04fe4ac55a80d5ce703ecf4f1f
Chris <teamcanada61312outlook.com>
2022-03-06 12:30:19 -0500

    updated to use multiple websites

--- index.js
+++ index.js
@@ -6,19 +6,40 @@ const axios = require('axios')
 const app = express()
 
 // CHECK MULTIPLE SITES FOR NEWS LINKS
-//
-// const newsSites = [
-//     {
-//         name: 'Sporting News NHL',
-//         address: 'https://www.sportingnews.com/ca/nhl/news'
-//     },
-//     {
-//         name: 'Sporting News NFL',
-//         address: 'https://www.sportingnews.com/ca/nfl'
-//     }
-// ]
+
+const newsSites = [
+      {
+         name: 'Sporting News NHL',
+         address: 'https://www.sportingnews.com/ca/nhl/news'
+     },
+     {
+         name: 'Sporting News NFL',
+         address: 'https://www.nhl.com/news/'
+     }
+]
 
 const articles = []
+
+newsSites.forEach(newsSite => {
+    axios.get(newsSite.address)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        $('a:contains("Senators")', html).each(function () {
+            const title = $(this).text()
+        //  const url = $(this).find('.card__link').attr('href') --------- this will find a specific ID of the links
+            const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url,
+                    source: newsSite.name
+                })
+            })
+            console.log(articles)
+        })
+    })
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to my Sports News <br> You can view it <a href="/news">here</a>')
@@ -26,36 +47,9 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
 
-    const url1 = 'https://www.sportingnews.com/ca/nhl/news'
+    res.json(articles)
 
-    axios.get(url1)
-
-        .then((response) => {
-            const html = response.data
-            const $ = cheerio.load(html)
-
-
-            // WE CAN SEARCH SPECIFIC TITLES THAT CONTAIN THIS WAY:
-            //
-            // $('.card__body:contains("covid"), html).each(function() {
-            //      the rest goes here
-            // })
-
-
-            $('.card__body', html).each(function() {
-                const title = $(this).find('.card__headline--long').text()
-                const url = $(this).find('.card__link').attr('href')
-                articles.push({
-                    title,
-                    url
-                })
-            })
-            console.log(articles)
-            res.json(articles)
-            
-        }).catch((err) => console.log(err))
-
-    })
+})
 
 
 
